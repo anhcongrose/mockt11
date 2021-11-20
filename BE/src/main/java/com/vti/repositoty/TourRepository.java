692b0d06a93e216661a9a47887fe607b90be3305
anhcongrose <anhcongrose@gmail.com>
2021-11-20 22:34:42 +0700

    comit-20-11

---
+++ BE/src/main/java/com/vti/repositoty/TourRepository.java
@@ -0,0 +1,11 @@
+package com.vti.repositoty;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import com.vti.entity.Tour;
+
+public interface TourRepository extends JpaRepository<Tour, Short>, JpaSpecificationExecutor<Tour> {
+	
+
+}
---
+++ BE/src/main/java/com/vti/service/TourService.java
@@ -0,0 +1,31 @@
+package com.vti.service;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import com.vti.entity.Tour;
+import com.vti.repositoty.TourRepository;
+
+@Service
+public class TourService implements ITourService {
+
+	@Autowired
+	private TourRepository repository;
+
+	@Override
+	public Page<Tour> getAllTours(Pageable pageable) {
+		// TODO Auto-generated method stub
+		return repository.findAll(pageable);
+	}
+
+
+	@Override
+	public Tour  getTourById(short tourId) {	
+		return repository.findById(tourId).get();
+	}
+
+}
